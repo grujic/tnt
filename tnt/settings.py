6a6a639af2df6698c2657a90465fb91fe803d914
tom <thomas.grujic@gmail.com>
2014-08-01 10:21:38 +0200

    Added path for where collected static is stored

--- tnt/settings.py
+++ tnt/settings.py
@@ -89,6 +89,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_PATH, 'collectedstatic/')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
