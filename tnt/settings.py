744b9f500cd03aed664d2701c37551e50391c5ba
Tom <thomas.grujic@gmail.com>
2014-08-01 09:32:58 +0100

    removed gunicorn from installed apps, not needed

--- tnt/settings.py
+++ tnt/settings.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
