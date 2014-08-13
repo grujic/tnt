222ba3c589e6534f61fb3282b4d71d3acfa76f2a
sal-assam <sarah.ali.alassam@gmail.com>
2014-08-13 11:14:19 +0100

    Update initial_base_states.py
    Adding a couple of extra initial spin base states: domain wall and alternating spins case.

--- tnt/initial_base_states.py
+++ tnt/initial_base_states.py
@@ -5,7 +5,7 @@ initial_base_states_list = \
 	# Spin system - all spins down
 	{
 		'initial_base_state_id': 1, \
-		'function_tex_str': "| 	\downarrow 	\downarrow \cdots \downarrow \rangle", \
+		'function_tex_str': "| 	\downarrow \downarrow \cdots \downarrow \rangle", \
 		'function_description': 'All spins down', \
 		'system_type': 'spin'
 	}, 
@@ -13,8 +13,24 @@ initial_base_states_list = \
 	# Spin system - all spins up
 	{
 		'initial_base_state_id': 2, \
-		'function_tex_str': '| 	\uparrow 	\uparrow \cdots \uparrow \rangle', \
+		'function_tex_str': '| 	\uparrow \uparrow \cdots \uparrow \rangle', \
 		'function_description': 'All spins up', \
+		'system_type': 'spin'
+	},
+	
+	# Spin system - left half spins up
+	{
+		'initial_base_state_id': 3, \
+		'function_tex_str': '| 	\uparrow \uparrow \cdots \uparrow \downarrow \downarrow \cdots \downarrow \rangle', \
+		'function_description': 'Left half spins up, right half spins down', \
+		'system_type': 'spin'
+	},
+	
+	# Spin system - alternating spins
+	{
+		'initial_base_state_id': 4, \
+		'function_tex_str': '| 	\uparrow \downarrow \uparrow \downarrow \cdots \uparrow \downarrow \rangle', \
+		'function_description': 'Alternating spins', \
 		'system_type': 'spin'
 	}
 
