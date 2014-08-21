bb1b4626596477124419d07e982794fa0059dec1
tom <thomas.grujic@gmail.com>
2014-08-21 12:51:10 +0200

    Made nicer sign in and signup pages

--- tnt/apps/dashboard/views.py
+++ tnt/apps/dashboard/views.py
@@ -3,28 +3,29 @@ from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.core.context_processors import csrf
 
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
+
 from django.http import HttpResponseRedirect
+
+from django.contrib.auth.decorators import login_required
 
 from userprofile.models import MyUser
 
 # Create your views here.
-def login(request):
-
-    return render(request, \
-                  'dashboard/login.html')
 
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/login/')
 
-def landing(request):
+def signup(request):
     state = "Please enter an email and password..."
     if request.method == 'POST':
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
 
-        print("username = ")
-        print(username)
+        print("email = ")
+        print(email)
 
         print("password = ")
         print(password)
@@ -34,7 +35,6 @@ def landing(request):
             MyUser.objects.create_user(email, password=password)
             user = authenticate(username=email, password=password)
             
-
             login(request, user)
             state = "You're successfully logged in!"
             return HttpResponseRedirect('/')
@@ -46,29 +46,31 @@ def landing(request):
     args.update(csrf(request))
     args['state'] = state
 
-    return render(request, 'dashboard/landing.html', args)
+    return render(request, 'dashboard/signup.html', args)
 
+@login_required
 def home(request):
     # Users's home page showing previous calculations and ability to create a new one
     return render(request, \
                   'dashboard/home.html')
 
+@login_required
 def new_calculation(request):
     # Page to define a new calculation
     return render(request, \
                   'dashboard/new_calculation.html')
 
+@login_required
 def calculations(request):
     # See all your calculations
     return render(request, \
                   'dashboard/calculations.html')
 
+@login_required
 def explore_calculation(request, calculation_id):
     # Explore a calculation's expectation values etc in the browser
     return render(request, \
                   'dashboard/explore_calculation.html')
-
-
 
 def faq(request):
     # Page for FAQ
