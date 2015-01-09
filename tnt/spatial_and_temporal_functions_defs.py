04f01ee7a2bcb4563d7d9f43c2f6c24e1e88521b
sal-assam <sarah.ali.alassam@gmail.com>
2015-01-09 19:21:53 +0000

    Simplifying layout of initial state page, and changing some operator definitions to be clearer.

--- tnt/spatial_and_temporal_functions_defs.py
+++ tnt/spatial_and_temporal_functions_defs.py
@@ -63,7 +63,7 @@ fns = \
 	# Quadratic
 	{
 		'id': 3, \
-		'function_tex_str': 'a + b(j-c)^2', \
+		'function_tex_str': 'a + b(j-j_c)^2', \
 		'function_description': 'Quadratic', \
 		'use_for_spatial_hamil': True, \
 		'use_for_temporal_hamil': True, \
@@ -156,7 +156,7 @@ fns = \
 	# Rectangular function
 	{
 		'id': 6, \
-		'function_tex_str': 'A \mathrm{rect}((j-c)/w)', \
+		'function_tex_str': 'A \mathrm{rect}((j-j_c)/w)', \
 		'function_description': 'Rectangular function', \
 		'use_for_spatial_hamil': True, \
 		'use_for_temporal_hamil': True, \
@@ -187,7 +187,7 @@ fns = \
 	# Delta function
 	{
 		'id': 7, \
-		'function_tex_str': 'A \delta(j-c)', \
+		'function_tex_str': 'A \delta(j-j_c)', \
 		'function_description': 'Delta function', \
 		'use_for_spatial_hamil': True, \
 		'use_for_temporal_hamil': True, \
