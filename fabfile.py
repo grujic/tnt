f758988e4c99fa97f7bf7df51f256c4973096272
Tom Grujic <thomas.grujic@gmail.com>
2015-02-08 19:33:30 +0000

    Added a fabfile, installed fabric and updated the pip requirements.txt file

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from fabric.api import local, lcd
+import os
+import requests
+
+def deploy:
+    print("Adding and committing all changes...\n")
+    local("git add -A")
+    local("git commit")
+    local("git push")
+
+    with lcd("/webapps/tnt/")
+
+        print("Checking out dev branch...\n")
+        local("git checkout dev")
+        print("Pulling dev changes...\n")
+        local("git pull")
+        print("Checking out master again...\n")
+        local("git checkout master")
+        print("Merging master with dev...\n")
+        local("git merge dev")
+        print("Collecting static files...\n")
+        local("python manage.py collectstatic")
+        print("Restarting supervisord app...\n")
+        local("supervisorctl restart tnt")
+
+        resp = requests.get("http://www.tntgo.org") # First request to warm up the server
+
